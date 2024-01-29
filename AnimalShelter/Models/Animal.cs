527eff9f8d2440609412e14fe8ae05d6615874f2
ChrisRDavila <crossdavila@gmail.com>
2024-01-29 11:54:54 -0800

    update animal props to match db

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -2,6 +2,10 @@ namespace AnimalShelter.Models
 {
   public class Animal
   {
-    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public string Breed { get; set; }
+    public int Age { get; set; }
+    public int AnimalId { get; set; }
   }
 }
